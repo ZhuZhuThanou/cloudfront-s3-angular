88805e5d586aa1ffc629bb7684638ebd101fc62c
ZhuZhuThanou <blogicalbmath@gmail.com>
2021-07-06 21:55:15 -0400

    add deployment

--- lib/cloudfront-s3-angular-stack.ts
+++ lib/cloudfront-s3-angular-stack.ts
@@ -1,7 +1,9 @@
 import { BlockPublicAccess, Bucket } from "@aws-cdk/aws-s3";
 import * as cdk from "@aws-cdk/core";
+import * as s3deploy from '@aws-cdk/aws-s3-deployment';
 import * as cloudfront from '@aws-cdk/aws-cloudfront';
 import * as origins from '@aws-cdk/aws-cloudfront-origins';
+import * as path from 'path';
 
 export class CloudfrontS3AngularStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -21,6 +23,11 @@ export class CloudfrontS3AngularStack extends cdk.Stack {
       errorResponses: [{ httpStatus:404, 
         responseHttpStatus: 200, 
         responsePagePath: '/index.html'}]
+    });
+
+    new s3deploy.BucketDeployment(this, 'deployWeb', {
+      sources: [s3deploy.Source.asset(path.join(__dirname, '..', 'web-app', 'dist'))],
+      destinationBucket: bucket
     });
   }
 }
